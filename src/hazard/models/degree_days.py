53bc89ed1fe9325c3ff12aa8e584f2d42322cfd2
Joe Moorhouse <joe.moorhouse@gmail.com>
2023-01-02 23:06:15 +0000

    First commit: degree days model
    Signed-off-by: Joe Moorhouse <joe.moorhouse@gmail.com>

---
+++ src/hazard/models/degree_days.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass
+import os, logging
+from typing import Iterable
+
+from hazard.utilities.xarray_utilities import enforce_conventions
+from hazard.protocols import OpenDataset, WriteDataset
+
+from dask.distributed import Client, LocalCluster
+import numpy as np
+import xarray as xr
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class BatchItem():
+    gcm: str
+    scenario: str
+    central_year: int
+
+
+class DegreeDays:  
+    """Calculates degree days from temperature data sets."""
+
+    def __init__(self,
+            theshold: float=32,
+            window_years: int=20,
+            gcms: Iterable[str]=None,
+            scenarios: Iterable[str]=None,
+            central_years: Iterable[int]=None):
+        """Construct model to calculate degree days from temperature data sets.
+
+        Args:
+            theshold (float, optional): degree days above threshold are calculated. Defaults to 32.
+            window_years (int, optional): number of years for average. Defaults to 20.
+            gcms (Iterable[str], optional): Global Circulation Models to include in calculation. Defaults to None.
+            scenarios (Iterable[str], optional): scenarios to include in calculation. Defaults to None.
+            central_years (Iterable[int], optional): central years to include in calculation. Defaults to None.
+        """
+
+
+        self.threshold: float = 273.15 + theshold # in Kelvin; degree days above 32C
+        self.window_years = window_years
+        self.gcms = ["ACCESS-CM2", "CMCC-ESM2", "CNRM-CM6-1",
+            "MPI-ESM1-2-LR", "MIROC6", "NorESM2-MM"] if gcms is None else gcms
+        self.scenarios = ["historical", "ssp126", "ssp245", "ssp585"] if scenarios is None else scenarios
+        # 1995, 2014 (2010)
+        # 2021, 2040 (2030)
+        # 2031, 2050 (2040)
+        # 2041, 2060 (2050)
+        self.central_years = [2010, 2030, 2040, 2050] if central_years is None else central_years
+
+
+    def run(self, source: OpenDataset, target: WriteDataset, client: Client=None):
+        if (client is None):
+            cluster = LocalCluster(processes=False)
+            client = Client(cluster)
+        for item in self._batch_items():
+            average_deg_days = self._average_degree_days(client, source, target, item)
+            target.write(self._item_path(item), average_deg_days)
+
+
+    def _average_degree_days(self, client: Client, source: OpenDataset, target: WriteDataset, item: BatchItem):
+        """Caclulate average annual degree days for given window for the GCM and scenario specified."""
+        years = range(item.central_year - self.window_years // 2, item.central_year + self.window_years // 2 + (self.window_years % 2))
+        logging.info(f"Calculating average degree days, gcm={item.gcm}, scenario={item.scenario}, years={list(years)}")
+        futures = []
+        for year in years:
+            future = client.submit(self._degree_days, source, item.gcm, item.scenario, year)
+            futures.append(future)
+        deg_days = client.gather(futures)
+        average = sum(deg_days) / float(len(years))
+        return enforce_conventions(average)
+
+
+    def _batch_items(self) -> Iterable[BatchItem]:
+        """Items to process."""
+        for gcm in self.gcms:
+            for scenario in self.scenarios:
+                for central_year in self.central_years:
+                    yield BatchItem(gcm=gcm, scenario=scenario, central_year=central_year)    
+
+
+    def _degree_days(self, source: OpenDataset, gcm: str, scenario: str, year: int ) -> xr.DataArray:
+        """Calculate degree days for Dataset provided."""
+        # check DataArray 
+        with source.open_dataset_year(gcm, scenario, "tasmax", year) as ds:
+            if any(coord not in ds.coords.keys() for coord in ['lat', 'lon', 'time']):
+                raise ValueError("expect coordinates: 'lat', 'lon' and 'time'")
+            if (ds.time.dt.year != year).any():
+                raise ValueError("unexpected year found") 
+            # normalize to 365 days
+            scale = 365.0 / len(ds.time)
+            # will raise error if taxmax not present    
+            return scale * xr.where(ds.tasmax > self.threshold, ds.tasmax - self.threshold, 0).sum(dim=["time"])
+
+    
+    def _item_path(self, item: BatchItem) -> str:
+        path = "chronic_heat/osc/v2" # v2 uses downscaled data
+        typ = "mean_degree_days_v2" # need v2 in filename to make unique
+        levels = ['above', '32c']
+        return os.path.join(path, f"{typ}_{levels[0]}_{levels[1]}_{item.gcm}_{item.scenario}_{item.central_year}")
+
+
+
---
+++ src/hazard/sources/osc_zarr.py
@@ -0,0 +1,111 @@
+import os
+from typing import MutableMapping, Tuple
+from affine import Affine
+import numpy as np
+import s3fs
+from hazard.protocols import OpenDataset, WriteDataset
+import hazard.utilities.xarray_utilities as xarray_utilities
+import hazard.utilities.zarr_utilities as zarr_utilities
+import xarray as xr
+import zarr
+
+class OscZarr(OpenDataset, WriteDataset):
+    default_staging_bucket = "redhat-osc-physical-landing-647521352890"
+    
+    def __init__(self, bucket: str=default_staging_bucket, prefix: str="hazard", store: MutableMapping=None, s3: s3fs.S3File=None):
+        """For reading and writing to OSC Climate Zarr storage. If store is provided this is used, otherwise if S3File is provided, this is used.
+        Otherwise, store is created using credentials in environment variables.
+        
+        Args:
+            bucket: Name of S3 bucket.
+            root: Path to Zarr Group, i.e. objects are located in S3://{bucket}/{prefix}/hazard.zarr/{rest of key}.
+            store: If provided, Zarr will use this store.
+            s3: S3File to use if present and if store not provided. 
+        """
+        if store is None:
+            if s3 is None:
+                #zarr_utilities.load_dotenv() # to load environment variables
+                s3 = s3fs.S3FileSystem(anon=False, key=os.environ["OSC_S3_ACCESS_KEY"], secret=os.environ["OSC_S3_SECRET_KEY"])
+            
+            group_path = os.path.join(bucket, prefix, "hazard.zarr")
+            store = s3fs.S3Map(root=group_path, s3=s3, check=False)
+        
+        self.root = zarr.group(store=store) 
+
+
+    def read_numpy(self, path, index=0) -> Tuple[np.ndarray, Affine]:
+        """Read index as two dimensional numpy array and affine transform.
+        This is intended for small datasets, otherwise recommended to 
+        use xarray.open_zarr."""
+        z = self.root[path]
+        t = z.attrs["transform_mat3x3"]  # type: ignore
+        transform = Affine(t[0], t[1], t[2], t[3], t[4], t[5])
+        return z[index, :, :], transform
+
+
+    def if_exists_remove(self, path):
+        if path in self.root:
+            self.root.pop(path)
+
+
+    def write(self, path: str, da: xr.DataArray):
+        """Write DataArray to provided relative path."""
+        data, transform, crs = xarray_utilities.get_array_components(da)
+        z = self._zarr_create(path, da.shape, transform)
+        z[0, :, :] = data[:,:]
+
+
+    def read_floored(self, path, longitudes, latitudes):
+        z = self.root[path]
+        t = z.attrs["transform_mat3x3"]  # type: ignore
+        transform = Affine(t[0], t[1], t[2], t[3], t[4], t[5])
+        index_values = z.attrs.get("index_values", [0])
+        if index_values is None:
+            index_values = [0]    
+        image_coords = OscZarr._get_coordinates(longitudes, latitudes, transform)
+        image_coords = np.floor(image_coords).astype(int)
+        iz = np.tile(np.arange(z.shape[0]), image_coords.shape[1])  # type: ignore
+        iy = np.repeat(image_coords[1, :], len(index_values))
+        ix = np.repeat(image_coords[0, :], len(index_values))
+        data = z.get_coordinate_selection((iz, iy, ix))  # type: ignore
+        return data
+
+
+    @staticmethod
+    def _get_coordinates(longitudes, latitudes, transform: Affine):
+        coords = np.vstack((longitudes, latitudes, np.ones(len(longitudes))))  # type: ignore
+        inv_trans = ~transform
+        mat = np.array(inv_trans).reshape(3, 3)
+        frac_image_coords = mat @ coords
+        return frac_image_coords
+
+
+    def _zarr_create(self, path, shape, transform, overwrite=False, return_periods=None):
+        """
+        Create Zarr array with given shape and affine transform.
+        """
+
+        z = self.root.create_dataset(
+            path,
+            shape=(1 if return_periods is None else len(return_periods), shape[0], shape[1]),
+            chunks=(1 if return_periods is None else len(return_periods), 1000, 1000),
+            dtype="f4",
+            overwrite=overwrite,
+        )  # array_path interpreted as path within group
+        trans_members = [
+            transform.a,
+            transform.b,
+            transform.c,
+            transform.d,
+            transform.e,
+            transform.f,
+        ]
+        mat3x3 = [x * 1.0 for x in trans_members] + [0.0, 0.0, 1.0]
+        z.attrs["transform_mat3x3"] = mat3x3
+        if return_periods is not None:
+            z.attrs["index_values"] = return_periods
+            z.attrs["index_name"] = "return period (years)"
+        return z
+
+
+
