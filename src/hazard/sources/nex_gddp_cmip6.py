b37c89169cfed4f7105961286d940106facd441c
Joe Moorhouse <joe.moorhouse@gmail.com>
2023-04-13 18:50:19 +0100

    Add grid label
    Signed-off-by: Joe Moorhouse <joe.moorhouse@gmail.com>

--- src/hazard/sources/nex_gddp_cmip6.py
+++ src/hazard/sources/nex_gddp_cmip6.py
@@ -33,13 +33,14 @@ class NexGddpCmip6(OpenDataset):
         """
         # subset of General Circulation Models (GCMs) and Variant IDs for analysis
         self.subset: Dict[str, Dict[str, str]] = {
-            "ACCESS-CM2":       {"variantId": "r1i1p1f1"},
-            "CMCC-ESM2":        {"variantId": "r1i1p1f1"},
-            "CNRM-CM6-1":       {"variantId": "r1i1p1f2"},
-            "MPI-ESM1-2-LR":    {"variantId": "r1i1p1f1"},
-            "MIROC6":           {"variantId": "r1i1p1f1"},
-            "NorESM2-MM":       {"variantId": "r1i1p1f1"},
+            "ACCESS-CM2":       {"variant_label": "r1i1p1f1", "grid_label": "gn"},
+            "CMCC-ESM2":        {"variant_label": "r1i1p1f1", "grid_label": "gn"},
+            "CNRM-CM6-1":       {"variant_label": "r1i1p1f2", "grid_label": "gr"}, 
+            "MPI-ESM1-2-LR":    {"variant_label": "r1i1p1f1", "grid_label": "gn"},
+            "MIROC6":           {"variant_label": "r1i1p1f1", "grid_label": "gn"},
+            "NorESM2-MM":       {"variant_label": "r1i1p1f1", "grid_label": "gn"},
         }
+        #<variable_id>_<table_id>_<source_id>_<experiment_id>_<variant_label>_<grid_label>_<time_range>.nc
         self.fs = s3fs.S3FileSystem(anon=True) if fs is None else fs
         self.root = NexGddpCmip6.bucket if root is None else root
         self.quantities = { "tas": {"name": "Daily average temperature"} }
@@ -47,9 +48,10 @@ class NexGddpCmip6(OpenDataset):
 
     def path(self, gcm="NorESM2-MM", scenario="ssp585", quantity="tas", year=2030):
         component = self.subset[gcm]
-        variantId = component["variantId"]
-        filename = f"{quantity}_day_{gcm}_{scenario}_{variantId}_gn_{year}.nc"
-        return (posixpath.join(self.root, f"NEX-GDDP-CMIP6/{gcm}/{scenario}/{variantId}/{quantity}/") + filename, filename)
+        variant_label = component["variant_label"]
+        grid_label = component["grid_label"]
+        filename = f"{quantity}_day_{gcm}_{scenario}_{variant_label}_{grid_label}_{year}.nc"
+        return (posixpath.join(self.root, f"NEX-GDDP-CMIP6/{gcm}/{scenario}/{variant_label}/{quantity}/") + filename, filename)
 
 
     def gcms(self) -> List[str]:
