5c4b7fe837bf048c28abe8e9c2059e680e724ba8
Joe Moorhouse <joe.moorhouse@gmail.com>
2023-09-25 12:38:55 +0100

    Fix typo
    Signed-off-by: Joe Moorhouse <joe.moorhouse@gmail.com>

--- src/hazard/utilities/s3_utilities.py
+++ src/hazard/utilities/s3_utilities.py
@@ -47,7 +47,7 @@ def copy_prod_to_public(prefix: str, dry_run = False):
     s3_source_client = boto3.client('s3', aws_access_key_id=os.environ["OSC_S3_ACCESS_KEY"], 
                                     aws_secret_access_key=os.environ["OSC_S3_SECRET_KEY"])
     s3_target_client = boto3.client('s3', aws_access_key_id=os.environ["OSC_S3_ACCESS_KEY_PUBLIC"], 
-                                    aws_secret_access_key=os.environ["OSC_S3_SECRET_KEY_PUBLiC"])
+                                    aws_secret_access_key=os.environ["OSC_S3_SECRET_KEY_PUBLIC"])
 
     source_bucket_name = os.environ["OSC_S3_BUCKET"]
     target_bucket_name = os.environ["OSC_S3_BUCKET_PUBLIC"]
