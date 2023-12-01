a1deeaeb0b720d3c27c91ed3c068975e50bb322a
EglantineGiraud <eglantine.giraud@gmail.com>
2023-12-01 19:34:52 +0100

    On-boarding of water temperature
    On-boarding of water temperature
    
    Signed-off-by: Eglantine Giraud <eglantine.giraud@gmail.com>
    Signed-off-by: EglantineGiraud <eglantine.giraud@gmail.com>

--- src/hazard/models/multi_year_average.py
+++ src/hazard/models/multi_year_average.py
@@ -1,24 +1,29 @@
+import logging
 from abc import abstractmethod
 from contextlib import ExitStack
 from dataclasses import dataclass
-import logging
 from pathlib import PurePosixPath
 from typing import Iterable, List, Optional, Tuple, TypeVar
 
+import xarray as xr
+from dask.distributed import Client
+from rasterio.crs import CRS  # type: ignore
+
 from hazard.indicator_model import IndicatorModel
 from hazard.inventory import HazardResource, MapInfo
+from hazard.protocols import (
+    Averageable,
+    OpenDataset,
+    ReadWriteDataArray,
+    WriteDataArray,
+)
 from hazard.utilities.map_utilities import generate_map
-
 from hazard.utilities.xarray_utilities import enforce_conventions_lat_lon
-from hazard.protocols import Averageable, OpenDataset, ReadWriteDataArray, WriteDataArray
-
-from dask.distributed import Client
-from rasterio.crs import CRS # type: ignore
-import xarray as xr
 
 logger = logging.getLogger(__name__)
 
-T = TypeVar('T', bound=Averageable)
+T = TypeVar("T", bound=Averageable)
+
 
 @dataclass
 class Indicator:
@@ -27,31 +32,45 @@ class Indicator:
     bounds: List[Tuple[float, float]]
 
 
-class MultiYearAverageIndicatorBase(IndicatorModel[T]):  
+class MultiYearAverageIndicatorBase(IndicatorModel[T]):
     """Indicator which is the average of indicators produced for a number of individual years.
     Such calculations can be split by year and run in parallel.
     """
 
-    _default_window_years: int=20
-    _default_gcms: Iterable[str]=["ACCESS-CM2", "CMCC-ESM2", "CNRM-CM6-1", "MPI-ESM1-2-LR", "MIROC6", "NorESM2-MM"]
-    _default_scenarios: Iterable[str]=["historical", "ssp126", "ssp245", "ssp585"]
+    _default_window_years: int = 20
+    _default_gcms: Iterable[str] = [
+        "ACCESS-CM2",
+        "CMCC-ESM2",
+        "CNRM-CM6-1",
+        "MPI-ESM1-2-LR",
+        "MIROC6",
+        "NorESM2-MM",
+    ]
+    _default_scenarios: Iterable[str] = ["historical", "ssp126", "ssp245", "ssp585"]
     _default_central_year_historical: int = 2005
-    _default_central_years: Iterable[int]=[2030, 2040, 2050] 
+    _default_central_years: Iterable[int] = [2030, 2040, 2050]
 
-    def __init__(self,
-            window_years: int=20,
-            gcms: Iterable[str]=_default_gcms,
-            scenarios: Iterable[str]=_default_scenarios,
-            central_year_historical: int =_default_central_year_historical,
-            central_years: Iterable[int]=_default_central_years): 
+    def __init__(
+        self,
+        window_years: int = 20,
+        gcms: Iterable[str] = _default_gcms,
+        scenarios: Iterable[str] = _default_scenarios,
+        central_year_historical: int = _default_central_year_historical,
+        central_years: Iterable[int] = _default_central_years,
+    ):
         """Construct model to calculate degree days from temperature data sets.
 
         Args:
             window_years (int, optional): Number of years for average. Defaults to 20.
-            gcms (Iterable[str], optional): Global Circulation Models to include in calculation. Defaults to ["ACCESS-CM2", "CMCC-ESM2", "CNRM-CM6-1", "MPI-ESM1-2-LR", "MIROC6", "NorESM2-MM"].
-            scenarios (Iterable[str], optional): Scenarios to include in calculation. Defaults to ["historical", "ssp126", "ssp245", "ssp585"].
-            central_year_historical (int): Central year to include in calculation for historical scenario. Defaults to 2005.
-            central_years (Iterable[int], optional): Central years to include in calculation. Defaults to [2010, 2030, 2040, 2050].
+            gcms (Iterable[str], optional): Global Circulation Models to include in calculation.
+                                            Defaults to ["ACCESS-CM2", "CMCC-ESM2", "CNRM-CM6-1",
+                                            "MPI-ESM1-2-LR", "MIROC6", "NorESM2-MM"].
+            scenarios (Iterable[str], optional): Scenarios to include in calculation.
+                                                 Defaults to ["historical", "ssp126", "ssp245", "ssp585"].
+            central_year_historical (int): Central year to include in calculation for historical scenario.
+                                           Defaults to 2005.
+            central_years (Iterable[int], optional): Central years to include in calculation.
+                                                     Defaults to [2010, 2030, 2040, 2050].
         """
 
         # 1995 to 2014 (2010), 2021 tp 2040 (2030), 2031 to 2050 (2040), 2041 to 2060 (2050)
@@ -60,59 +79,93 @@ class MultiYearAverageIndicatorBase(IndicatorModel[T]):
         self.scenarios = scenarios
         self.central_years = central_years
         self.central_year_historical = central_year_historical
-        
-    def run_single(self, item: T, source: OpenDataset, target: ReadWriteDataArray, client: Client):
+
+    def run_single(
+        self, item: T, source: OpenDataset, target: ReadWriteDataArray, client: Client
+    ):
         averaged_indicators = self._averaged_indicators(client, source, target, item)
         for indicator in averaged_indicators:
             indicator.array.attrs["crs"] = CRS.from_epsg(4326)
             logger.info(f"Writing array to {str(indicator.path)}")
             target.write(str(indicator.path), indicator.array)
             path_map = indicator.path.with_name(indicator.path.name + "_map")
-            self._generate_map(str(indicator.path), str(path_map), indicator.bounds, target)
+            self._generate_map(
+                str(indicator.path), str(path_map), indicator.bounds, target
+            )
         return
-    
-    def _generate_map(self, path: str, map_path: str, bounds: Optional[List[Tuple[float, float]]], target: ReadWriteDataArray):
+
+    def _generate_map(
+        self,
+        path: str,
+        map_path: str,
+        bounds: Optional[List[Tuple[float, float]]],
+        target: ReadWriteDataArray,
+    ):
         generate_map(path, map_path, bounds, target)
         return
-    
-    def _averaged_indicators(self, client: Client, source: OpenDataset, target: WriteDataArray, item: Averageable) -> List[Indicator]:
+
+    def _years(self, _: OpenDataset, item: Averageable) -> List[int]:
+        return range(
+            item.central_year - self.window_years // 2,
+            item.central_year + self.window_years // 2 + (self.window_years % 2),
+        )
+
+    def _averaged_indicators(
+        self,
+        client: Client,
+        source: OpenDataset,
+        target: WriteDataArray,
+        item: Averageable,
+    ) -> List[Indicator]:
         """Calculate average annual degree days for given window for the GCM and scenario specified."""
-        years = range(item.central_year - self.window_years // 2, item.central_year + self.window_years // 2 + (self.window_years % 2))
-        logger.info(f"Calculating average indicator for batch item {str(item)}, years={list(years)}")
+        years = self._years(source, item)
+        logger.info(
+            f"Calculating average indicator for batch item {str(item)}, years={list(years)}"
+        )
         futures = []
         for year in years:
-            future = client.submit(self._calculate_single_year_indicators, source, item, year)
+            future = client.submit(
+                self._calculate_single_year_indicators, source, item, year
+            )
             futures.append(future)
-        single_year_sets: List[List[Indicator]] = list(client.gather(futures)) # indicators for each year
-        indics_per_year = len(single_year_sets[0]) # number of indicators for each year
+        single_year_sets: List[List[Indicator]] = list(
+            client.gather(futures)
+        )  # indicators for each year
+        indics_per_year = len(single_year_sets[0])  # number of indicators for each year
         res: List[Indicator] = []
         for i in range(indics_per_year):
             average = sum(set[i].array for set in single_year_sets) / float(len(years))
-            assert isinstance(average, xr.DataArray) # must be non-zero
-            res.append(Indicator(array=enforce_conventions_lat_lon(average), 
-                                 path=single_year_sets[0][i].path,
-                                 bounds=single_year_sets[0][i].bounds))
+            assert isinstance(average, xr.DataArray)  # must be non-zero
+            res.append(
+                Indicator(
+                    array=enforce_conventions_lat_lon(average),
+                    path=single_year_sets[0][i].path,
+                    bounds=single_year_sets[0][i].bounds,
+                )
+            )
         return res
 
     @abstractmethod
     def batch_items(self) -> Iterable[T]:
         """Get batch items (batch items can be calculated independently from one another)."""
-        ...  
-    
+        ...
+
     @abstractmethod
     def inventory(self) -> Iterable[HazardResource]:
         """Get the inventory."""
         ...
 
     @abstractmethod
-    def _calculate_single_year_indicators(self, source: OpenDataset, item: T, year: int) -> List[Indicator]:
-        """Calculate indicators for a single year for a single batch item. If just a single indicator per batch, a list
-        of length one is expected."""
+    def _calculate_single_year_indicators(
+        self, source: OpenDataset, item: T, year: int
+    ) -> List[Indicator]:
+        """Calculate indicators for a single year for a single batch item.
+        If just a single indicator per batch, a list of length one is expected."""
         ...
 
 
 @dataclass
-class BatchItem():
+class BatchItem:
     resource: HazardResource
     gcm: str
     scenario: str
@@ -123,22 +176,32 @@ class BatchItem():
 
 
 class ThresholdBasedAverageIndicator(MultiYearAverageIndicatorBase[BatchItem]):
-    
     def batch_items(self) -> Iterable[BatchItem]:
         """Get batch items (batch items can be calculated independently from one another)."""
         resource = self._resource()
         for gcm in self.gcms:
             for scenario in self.scenarios:
-                central_years = [self.central_year_historical] if scenario == "historical" else self.central_years
+                central_years = (
+                    [self.central_year_historical]
+                    if scenario == "historical"
+                    else self.central_years
+                )
                 for central_year in central_years:
-                    yield BatchItem(resource=resource, gcm=gcm, scenario=scenario, central_year=central_year)    
-    
+                    yield BatchItem(
+                        resource=resource,
+                        gcm=gcm,
+                        scenario=scenario,
+                        central_year=central_year,
+                    )
+
     def inventory(self) -> Iterable[HazardResource]:
         """Get the inventory item(s)."""
-        return [self._resource()] #.expand()
+        return [self._resource()]  # .expand()
 
-    def _get_indicators(self, item: BatchItem, data_arrays: List[xr.DataArray], param: str) -> List[Indicator]:
-        """Find the 
+    def _get_indicators(
+        self, item: BatchItem, data_arrays: List[xr.DataArray], param: str
+    ) -> List[Indicator]:
+        """Find the
 
         Args:
             item (BatchItem): _description_
@@ -149,11 +212,23 @@ class ThresholdBasedAverageIndicator(MultiYearAverageIndicatorBase[BatchItem]):
             List[Indicator]: _description_
         """
         resource = item.resource
-        paths = [item.resource.path.format(temp_c=threshold, gcm=item.gcm, scenario=item.scenario, year=item.central_year)
-                 for threshold in resource.params[param]]
-        assert isinstance(resource.map, MapInfo)      
-        return [Indicator(array=array, path=PurePosixPath(paths[i]), bounds=resource.map.bounds) for i, array in enumerate(data_arrays)]
+        paths = [
+            item.resource.path.format(
+                temp_c=threshold,
+                gcm=item.gcm,
+                scenario=item.scenario,
+                year=item.central_year,
+            )
+            for threshold in resource.params[param]
+        ]
+        assert isinstance(resource.map, MapInfo)
+        return [
+            Indicator(
+                array=array, path=PurePosixPath(paths[i]), bounds=resource.map.bounds
+            )
+            for i, array in enumerate(data_arrays)
+        ]
 
     @abstractmethod
     def _resource(self) -> HazardResource:
-       ...
+        ...
--- src/hazard/models/water_temp.py
+++ src/hazard/models/water_temp.py
@@ -4,6 +4,7 @@
 import logging
 import os
 from contextlib import ExitStack
+from pathlib import PurePosixPath
 from typing import Iterable, List
 
 import requests  # type: ignore
@@ -12,7 +13,7 @@ from attr import dataclass
 
 from hazard.inventory import Colormap, HazardResource, MapInfo, Scenario
 from hazard.models.multi_year_average import Indicator, ThresholdBasedAverageIndicator
-from hazard.protocols import OpenDataset
+from hazard.protocols import Averageable, OpenDataset
 
 logger = logging.getLogger(__name__)
 
@@ -43,14 +44,8 @@ class FutureStreamsSource(OpenDataset):
         self.from_years = [2006, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090]
         self.to_years = [2019, 2029, 2039, 2049, 2059, 2069, 2079, 2089, 2099]
 
-    @staticmethod
-    def adjust_from_year(gcm, scenario, from_year):
-        if gcm == "historical" and scenario == "E2O":
-            return 1979
-        return from_year
-
     def open_dataset_year(
-        self, gcm: str, scenario: str, quantity: str, year: int, chunks=None
+        self, gcm: str, scenario: str, _: str, year: int, chunks=None
     ) -> xr.Dataset:
         from_years = (
             self.from_historical_years if scenario == "historical" else self.from_years
@@ -63,25 +58,33 @@ class FutureStreamsSource(OpenDataset):
             for from_year, to_year in zip(from_years, to_years)
             if year >= from_year and year <= to_year
         )
-        filename, url = self.water_temp_download_path(
+        path, url = self.water_temp_download_path(
             gcm, scenario, set_from_year, set_to_year
         )
-        self.download_file(url, os.path.join(self.working_dir, filename))
-        return xr.open_dataset(filename)
+        self.download_file(url, path)
+        return xr.open_dataset(path, chunks=chunks)
 
     def water_temp_download_path(
         self, gcm: str, scenario: str, from_year: int, to_year: int
     ):
+        adjusted_gcm = gcm if gcm == "E2O" else gcm.lower()
+        adjusted_scenario = scenario[:4] if scenario == "historical" else scenario
         filename = self.filename(gcm, scenario, from_year, to_year)
         url = (
             "https://geo.public.data.uu.nl/vault-futurestreams/research-futurestreams%5B1633685642%5D/"
-            + f"original/waterTemp/{scenario}/{gcm}/"
+            + f"original/waterTemp/{adjusted_scenario}/{adjusted_gcm}/"
             + filename
         )
-        return filename, url
+        return os.path.join(self.working_dir, filename), url
 
     def filename(self, gcm: str, scenario: str, from_year: int, to_year: int):
-        adjusted_from_year = self.adjust_from_year(gcm, scenario, from_year)
+        adjusted_from_year = (
+            1979
+            if scenario == "historical"
+            and gcm == "E2O"
+            and from_year == self.from_historical_years[0]
+            else from_year
+        )
         from_date = f"{adjusted_from_year}-01-07"
         to_date = f"{to_year}-12-30"
         adjusted_gcm = gcm if gcm == "E2O" else gcm.lower()
@@ -112,7 +115,23 @@ class FutureStreamsSource(OpenDataset):
 class WaterTemperatureIndicator(ThresholdBasedAverageIndicator):
     def __init__(
         self,
-        threshold_temps_c: List[float] = [25, 30, 35, 40, 45, 50, 55],
+        threshold_temps_c: List[float] = [
+            5,
+            7.5,
+            10,
+            12.5,
+            15,
+            17.5,
+            20,
+            22.5,
+            25,
+            27.5,
+            30,
+            32.5,
+            35,
+            37.5,
+            40,
+        ],
         window_years: int = 20,
         gcms: Iterable[str] = ["E2O", "GFDL", "HadGEM", "IPSL", "MIROC", "NorESM"],
         scenarios: Iterable[str] = [
@@ -123,7 +142,7 @@ class WaterTemperatureIndicator(ThresholdBasedAverageIndicator):
             "rcp8p5",
         ],
         central_year_historical: int = 1991,
-        central_years: Iterable[int] = [2030, 2040, 2050, 2060, 2070, 2080, 2090],
+        central_years: Iterable[int] = [2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090],
     ):
         super().__init__(
             window_years=window_years,
@@ -134,37 +153,67 @@ class WaterTemperatureIndicator(ThresholdBasedAverageIndicator):
         )
         self.threshold_temps_c = threshold_temps_c
 
+    def _years(self, source: OpenDataset, item: Averageable) -> List[int]:
+        if item.scenario == "historical":
+            return source.from_historical_years
+        return [
+            source.from_year
+            for (from_year, to_year) in zip(source.from_years, source.to_years)
+            if from_year == item.year or to_year + 1 == item.year
+        ]
+
     def _calculate_single_year_indicators(
         self, source: OpenDataset, item: BatchItem, year: int
     ) -> List[Indicator]:
         """For a single year and batch item calculate the indicators (i.e. one per threshold temperature)."""
         logger.info(f"Starting calculation for year {year}")
         with ExitStack() as stack:
-            tas = stack.enter_context(
-                source.open_dataset_year(item.gcm, item.scenario, "tas", year)
-            ).tas
-            results = self._weeks_tas_above_indicators(
-                tas, year, self.threshold_temps_c
-            )
+            dataset = stack.enter_context(
+                source.open_dataset_year(
+                    item.gcm,
+                    item.scenario,
+                    "waterTemperature",
+                    year,
+                    chunks={"time": -1, "latitude": 540, "longitude": 1080},
+                )
+            ).waterTemperature
+            results = self._weeks_water_temp_above_indicators(dataset)
         logger.info(f"Calculation complete for year {year}")
-        # Get Indicators for reach array, looking up path using "threshold"
-        return self._get_indicators(item, results, "temp_c")
+        return [
+            Indicator(
+                results,
+                PurePosixPath(
+                    self._resource.path.format(
+                        gcm=item.gcm, scenario=item.scenario, year=item.central_year
+                    )
+                ),
+                self.resources["above"].map.bounds,
+            )
+        ]
 
-    def _weeks_tas_above_indicators(
-        self, tas: xr.DataArray, year: int, threshold_temps_c: List[float]
+    def _weeks_water_temp_above_indicators(
+        self, input: xr.DataArray
     ) -> List[xr.DataArray]:
         """Create DataArrays containing indicators the thresholds for a single year."""
-        if any(coord not in tas.coords.keys() for coord in ["lat", "lon", "time"]):
-            raise ValueError("expect coordinates: 'lat', 'lon' and 'time'")
-        if (tas.time.dt.year != year).any():
-            raise ValueError("unexpected year found")
+        if any(
+            coord not in input.coords.keys()
+            for coord in ["latitude", "longitude", "time"]
+        ):
+            raise ValueError("expect coordinates: 'latitude', 'longitude' and 'time'")
         # normalize to 52 weeks
-        scale = 52.0 / len(tas.time)
-        # will raise error if tas not present
-        return [
-            scale * xr.where(tas > (273.15 + threshold_c), 1.0, 0.0).sum(dim=["time"])
-            for threshold_c in threshold_temps_c
-        ]
+        scale = 52.0 / len(input.time)
+        coords = {
+            "index": self.threshold_temps_c,
+            "lat": input.coords["latitude"].values,
+            "lon": input.coords["longitude"].values,
+        }
+        output = xr.DataArray(coords=coords, dims=coords.keys())
+        for i, threshold_c in enumerate(self.threshold_temps_c):
+            threshold_k = 273.15 + threshold_c
+            output[i, :, :] = scale * xr.where(input > threshold_k, 1.0, 0.0).sum(
+                dim=["time"]
+            )
+        return input
 
     def _resource(self) -> HazardResource:
         """Create resource."""
@@ -172,18 +221,18 @@ class WaterTemperatureIndicator(ThresholdBasedAverageIndicator):
             description = f.read()
         resource = HazardResource(
             hazard_type="ChronicHeat",
-            indicator_id="weeks_water_temp/above/{temp_c}c",
+            indicator_id="weeks_water_temp_above",
             indicator_model_id=None,
             indicator_model_gcm="{gcm}",
             params={
                 "temp_c": [str(t) for t in self.threshold_temps_c],
                 "gcm": list(self.gcms),
             },
-            path="chronic_heat/osc/v2/weeks_water_temp_above_{temp_c}c_{gcm}_{scenario}_{year}",
-            display_name="Weeks with average temperature above {temp_c}°C/{gcm}",
+            path="chronic_heat/osc/v2/weeks_water_temp_above_{gcm}_{scenario}_{year}",
+            display_name="Weeks with average temperature above threshold in degrees celsius/{gcm}",
             description=description,
             display_groups=[
-                "Weeks with average temperature above"
+                "Weeks with average temperature above threshold in degrees celsius"
             ],  # display names of groupings
             group_id="",
             map=MapInfo(
@@ -196,8 +245,8 @@ class WaterTemperatureIndicator(ThresholdBasedAverageIndicator):
                     max_index=255,
                     units="weeks/year",
                 ),
-                bounds=[(-180.0, 85.0), (180.0, 85.0), (180.0, -60.0), (-180.0, -60.0)],
-                path="weeks_tas_above_{temp_c}c_{gcm}_{scenario}_{year}_map",
+                bounds=[(-180.0, 90.0), (180.0, 90.0), (180.0, -90.0), (-180.0, -90.0)],
+                path="weeks_water_temp_above_{gcm}_{scenario}_{year}_map",
                 source="map_array",
             ),
             units="weeks/year",
