8bde3eb934c996145d7f430e89f82c06ec0410d7
EglantineGiraud <eglantine.giraud@gmail.com>
2023-12-02 15:18:07 +0100

    On-boarding of water temperature
    On-boarding of water temperature
    
    Signed-off-by: Eglantine Giraud <eglantine.giraud@gmail.com>
    Signed-off-by: EglantineGiraud <eglantine.giraud@gmail.com>

--- src/hazard/models/multi_year_average.py
+++ src/hazard/models/multi_year_average.py
@@ -179,7 +179,11 @@ class ThresholdBasedAverageIndicator(MultiYearAverageIndicatorBase[BatchItem]):
         """Get batch items (batch items can be calculated independently from one another)."""
         resource = self._resource()
         for gcm in self.gcms:
-            scenarios = ["historical"] if gcm == "E2O" else self.scenarios
+            scenarios = (
+                [scenario for scenario in self.scenarios if scenario == "historical"]
+                if gcm == "E2O"
+                else self.scenarios
+            )
             for scenario in scenarios:
                 central_years = (
                     [self.central_year_historical]
