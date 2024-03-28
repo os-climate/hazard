5e2017f7b926dad2b4649c8b3f4e4c8d9f990f9d
emileten <e.tenezakis@gmail.com>
2024-03-28 21:21:57 +0300

    make use of PC stac collection to pull CMIP6 paths

---
+++ src/hazard/utilities/stac_utilities.py
@@ -0,0 +1,9 @@
+from pystac.item_collection import ItemCollection
+from pystac_client import Client
+from typing import Dict
+
+def search_stac_items(catalog_url: str, search_params: Dict) -> ItemCollection:
+    client = Client.open(catalog_url)
+    search = client.search(**search_params)
+    items = search.item_collection()
+    return items
