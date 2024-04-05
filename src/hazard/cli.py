3e289a20d7f1044188147513c27513b072445a28
emileten <e.tenezakis@gmail.com>
2024-04-05 18:01:48 +0300

    lint

--- src/hazard/cli.py
+++ src/hazard/cli.py
@@ -1,6 +1,8 @@
+import logging  # noqa: E402
+
 import fire
 from dask.distributed import Client, LocalCluster  # noqa: E402
-import logging  # noqa: E402
+
 from hazard.docs_store import DocStore  # type: ignore # noqa: E402
 from hazard.models.days_tas_above import DaysTasAboveIndicator  # noqa: E402
 from hazard.sources.nex_gddp_cmip6 import NexGddpCmip6  # noqa: E402
@@ -11,28 +13,23 @@ logging.basicConfig(
     format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
 )
 
-def days_tas_above_indicator(bucket: str,
-                             prefix: str,
-                             gcm: str,
-                             scenario: str,
-                             year: int,
-                             threshold: int):
-    
+
+def days_tas_above_indicator(bucket: str, prefix: str, gcm: str, scenario: str, year: int, threshold: int):
     """
     Run the days_tas_above indicator generation for a given gcm, scenario, year and temperature threshold.
     Store the result in a zarr store located in a given bucket and prefix.
     """
-    
+
     docs_store = DocStore(prefix="hazard_test")
 
     cluster = LocalCluster(processes=False)
-    
+
     client = Client(cluster)
-    
+
     source = NexGddpCmip6()
-    
+
     target = OscZarr(bucket=bucket, prefix=prefix)
-    
+
     model = DaysTasAboveIndicator(
         threshold_temps_c=[threshold],
         window_years=1,
@@ -40,10 +37,11 @@ def days_tas_above_indicator(bucket: str,
         scenarios=[scenario],
         central_years=[year],
     )
-    
+
     docs_store.update_inventory(model.inventory())
-    
+
     model.run_all(source, target, client=client)
+
 
 if __name__ == "__main__":
     fire.Fire(days_tas_above_indicator)
