712b6823ad9a4d53e87a6bb74e44464c79e80163
EglantineGiraud <eglantine.giraud@gmail.com>
2024-07-07 09:40:10 +0200

    Add map of land subsidence rates
    Fix CI tests
    
    Signed-off-by: EglantineGiraud <eglantine.giraud@gmail.com>

--- src/hazard/inventory.py
+++ src/hazard/inventory.py
@@ -185,7 +185,7 @@ class HazardResource(BaseModel):
 class HazardResources(BaseModel):
     resources: List[HazardResource]
 
-    def to_stac_items(self, path_root: str, items_as_dicts: bool = False) -> List[Union[pystac.Item, Dict[str, Any]]]:
+    def to_stac_items(self, path_root: str, items_as_dicts: bool = False) -> List[Dict[str, Any]]:
         """
         converts hazard resources to a list of STAC items.
         """
