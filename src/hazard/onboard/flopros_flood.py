d8fdf53562410e595e004e1e74212b2b44cdad59
Joe Moorhouse <5102656+joemoorhouse@users.noreply.github.com>
2025-01-17 11:55:27 +0000

    Linting changes
    Signed-off-by: Joe Moorhouse <5102656+joemoorhouse@users.noreply.github.com>

--- src/hazard/onboard/flopros_flood.py
+++ src/hazard/onboard/flopros_flood.py
@@ -1,4 +1,3 @@
-
 import logging
 import os
 import math
@@ -30,7 +29,7 @@ logger = logging.getLogger(__name__)
 
 class FLOPROSFloodStandardOfProtectionSource(OpenDataset):
     def __init__(self, source_dir, fs: Optional[AbstractFileSystem] = None):
-        """Source that can provide FLOPROS data as an XArray raster. 
+        """Source that can provide FLOPROS data as an XArray raster.
 
         Args:
             source_dir (str): directory containing source files. If fs is a S3FileSystem instance
@@ -40,9 +39,7 @@ class FLOPROSFloodStandardOfProtectionSource(OpenDataset):
         """
         self.fs = fs if fs else LocalFileSystem()
         self.source_dir = source_dir
-        self.zip_url = (
-            "https://nhess.copernicus.org/articles/16/1049/2016/nhess-16-1049-2016-supplement.zip"
-        )
+        self.zip_url = "https://nhess.copernicus.org/articles/16/1049/2016/nhess-16-1049-2016-supplement.zip"
         self.archive_name = self.zip_url.split("/")[-1].split(".")[0]
         self.prepare()
 
@@ -50,26 +47,28 @@ class FLOPROSFloodStandardOfProtectionSource(OpenDataset):
         if not isinstance(self.fs, LocalFileSystem):
             # e.g. we are copying to S3;  download to specified working directory, but then copy to self.source_dir
             assert working_dir is not None
-            download_and_unzip(
-                self.zip_url, working_dir, self.archive_name
-            )
+            download_and_unzip(self.zip_url, working_dir, self.archive_name)
             for file in os.listdir(working_dir):
                 with open(file, "rb") as f:
                     self.fs.write_bytes(PurePosixPath(self.source_dir, file), f.read())
         else:
             # download and unzip directly in location
-            download_and_unzip(
-                self.zip_url, self.source_dir, self.archive_name
-            )
+            download_and_unzip(self.zip_url, self.source_dir, self.archive_name)
         logger.info("Reading database into GeoDataFrame")
-        path = Path(self.source_dir) / self.archive_name / "Scussolini_etal_Suppl_info"/ "FLOPROS_shp_V1" / "FLOPROS_shp_V1.shp"
+        path = (
+            Path(self.source_dir)
+            / self.archive_name
+            / "Scussolini_etal_Suppl_info"
+            / "FLOPROS_shp_V1"
+            / "FLOPROS_shp_V1.shp"
+        )
         self.df = gpd.read_file(path)
 
     def open_dataset_year(
         self, gcm: str, scenario: str, quantity: str, year: int, chunks=None
     ) -> xr.Dataset:
         hazard_type = quantity
-        
+
         def get_merged_rp(row, min_max: str, flood_type: str):
             """Calculate min or max from database entry (GeoDataFrame row).
 
@@ -86,38 +85,52 @@ class FLOPROSFloodStandardOfProtectionSource(OpenDataset):
                 float: Protection level as return period in years.
             """
             layers = ["DL", "PL", "ModL"] if flood_type == "Riv" else ["DL", "PL"]
-            for layer in layers: # design layer, policy layer, modelled layer
+            for layer in layers:  # design layer, policy layer, modelled layer
                 # note that for the modelled layer, both min and max are set to the modelled value
-                layer_rp = row[f"{layer}_{flood_type}"] if layer == "ModL" else row[f"{layer}_{min_max}_{flood_type}"]
-                if layer_rp > 0: # if 0, layer is considered missing
+                layer_rp = (
+                    row[f"{layer}_{flood_type}"]
+                    if layer == "ModL"
+                    else row[f"{layer}_{min_max}_{flood_type}"]
+                )
+                if layer_rp > 0:  # if 0, layer is considered missing
                     # if design layer is present, use this, otherwise use the policy layer, otherwise the modelled, otherwise missing
                     return layer_rp
-            return float("Nan") # zero is no data, represented by NaN here.
+            return float("Nan")  # zero is no data, represented by NaN here.
 
         logger.info(f"Processing hazard type {hazard_type}")
         min_shapes: List[Tuple[float, Any]] = []
         max_shapes: List[Tuple[float, Any]] = []
-        logger.info(f"Inferring max and min protection levels per region")
+        logger.info("Inferring max and min protection levels per region")
         for _, row in self.df.iterrows():
-            flood_type = "Riv" if hazard_type == "RiverineInundation" else "Co" # riverine and coastal        
-            min, max = get_merged_rp(row, "Min", flood_type), get_merged_rp(row, "Max", flood_type)
+            flood_type = (
+                "Riv" if hazard_type == "RiverineInundation" else "Co"
+            )  # riverine and coastal
+            min, max = (
+                get_merged_rp(row, "Min", flood_type),
+                get_merged_rp(row, "Max", flood_type),
+            )
             if row["name"] is None and (min is None and max is None):
                 continue
             # if either the min or max is NaN, that is OK: the vulnerability model is expected to deal with that
             if not math.isnan(min) and not math.isnan(max) and min > max:
                 # it can occur that for a layer there is only information about minimum
                 raise ValueError("unexpected return period")
-            
+
             if not math.isnan(min):
                 min_shapes.append((row.geometry, min))
             if not math.isnan(max):
                 max_shapes.append((row.geometry, max))
 
         resolution_in_arc_mins = 1
-        width, height = int(60 * 360 / resolution_in_arc_mins), int(60 * 180 / resolution_in_arc_mins) 
+        width, height = (
+            int(60 * 360 / resolution_in_arc_mins),
+            int(60 * 180 / resolution_in_arc_mins),
+        )
         crs, transform = global_crs_transform(width, height)
-        logger.info(f"Creating empty array")
-        da = empty_data_array(width, height, transform, str(crs), index_values = ["min", "max"])
+        logger.info("Creating empty array")
+        da = empty_data_array(
+            width, height, transform, str(crs), index_values=["min", "max"]
+        )
         # alternative by XArray preferred
         # z_sop = target.create_empty(
         #     resource.path,
@@ -129,20 +142,22 @@ class FLOPROSFloodStandardOfProtectionSource(OpenDataset):
         #     indexes=["min", "max"],
         # )
         for min_max in ["min", "max"]:
-            logger.info(f"Creating raster at {(360 * 60) / width} arcmin resolution for {min_max} protection")
+            logger.info(
+                f"Creating raster at {(360 * 60) / width} arcmin resolution for {min_max} protection"
+            )
             rasterized = features.rasterize(
                 min_shapes if min_max == "min" else max_shapes,
                 out_shape=[height, width],
                 transform=transform,
                 all_touched=True,
-                fill=float("nan"), # background value
+                fill=float("nan"),  # background value
                 merge_alg=MergeAlg.replace,
             )
             index = 0 if min_max == "min" else 1
             da[index, :, :] = rasterized[:, :]
-            #z_sop[index, :, :] = rasterized[:, :]
+            # z_sop[index, :, :] = rasterized[:, :]
         return da.to_dataset(name="sop")
-                    
+
 
 class FLOPROSFloodStandardOfProtection(IndicatorModel[str]):
     def __init__(self):
@@ -170,18 +185,18 @@ class FLOPROSFloodStandardOfProtection(IndicatorModel[str]):
 
     def batch_items(self):
         """Get a list of all batch items."""
-        return ["min_max"] # just one!
+        return ["min_max"]  # just one!
 
     def run_single(
         self, item: str, source: Any, target: ReadWriteDataArray, client: Client
     ):
-        assert isinstance(source, FLOPROSFloodStandardOfProtectionSource) 
-        logger.info(f"Writing rasters")
-        for hazard_type, resource  in self._resources().items():
+        assert isinstance(source, FLOPROSFloodStandardOfProtectionSource)
+        logger.info("Writing rasters")
+        for hazard_type, resource in self._resources().items():
             ds = source.open_dataset_year("", "", hazard_type, -1)
             assert ds
             target.write(resource.path, ds["sop"].compute(), spatial_coords=False)
-    
+
     def create_maps(self, source: OscZarr, target: OscZarr):
         """Create map images."""
         for resource in self.inventory():
@@ -203,40 +218,36 @@ class FLOPROSFloodStandardOfProtection(IndicatorModel[str]):
 
         return {
             k: HazardResource(
-                    hazard_type=k,
-                    indicator_id="flood_sop",
-                    indicator_model_id="flopros",
-                    indicator_model_gcm="",
-                    path="inundation/flopros_" + path_component(k) + "/v1/flood_sop/flood_sop",
-                    params={},
-                    display_name="Standard of protection (FLOPROS)",
-                    description=description,
-                    group_id="",
-                    display_groups=[],
-                    map=MapInfo(
-                        bbox=[],
-                        bounds=[],
-                        colormap=Colormap(
-                            max_index=255,
-                            min_index=1,
-                            nodata_index=0,
-                            name="flare",
-                            min_value=0.0,
-                            max_value=1500.0,
-                            units="years",
-                        ),
-                        index_values=None,
-                        path=f"maps/inundation/flopros_{path_component(k)}/v1/flood_sop_map",
-                        source="map_array_pyramid",
+                hazard_type=k,
+                indicator_id="flood_sop",
+                indicator_model_id="flopros",
+                indicator_model_gcm="",
+                path="inundation/flopros_"
+                + path_component(k)
+                + "/v1/flood_sop/flood_sop",
+                params={},
+                display_name="Standard of protection (FLOPROS)",
+                description=description,
+                group_id="",
+                display_groups=[],
+                map=MapInfo(
+                    bbox=[],
+                    bounds=[],
+                    colormap=Colormap(
+                        max_index=255,
+                        min_index=1,
+                        nodata_index=0,
+                        name="flare",
+                        min_value=0.0,
+                        max_value=1500.0,
+                        units="years",
                     ),
-                    units="years",
-                    scenarios=[
-                        Scenario(id="historical", years=[1985])
-                    ],
-                )
+                    index_values=None,
+                    path=f"maps/inundation/flopros_{path_component(k)}/v1/flood_sop_map",
+                    source="map_array_pyramid",
+                ),
+                units="years",
+                scenarios=[Scenario(id="historical", years=[1985])],
+            )
             for k in ["RiverineInundation", "CoastalInundation"]
         }
-    
-
-
-
--- tests/test_onboarding.py
+++ tests/test_onboarding.py
@@ -9,7 +9,10 @@ import s3fs
 import zarr
 import zarr.convenience
 from hazard.docs_store import DocStore
-from hazard.onboard.flopros_flood import FLOPROSFloodStandardOfProtection, FLOPROSFloodStandardOfProtectionSource
+from hazard.onboard.flopros_flood import (
+    FLOPROSFloodStandardOfProtection,
+    FLOPROSFloodStandardOfProtectionSource,
+)
 from hazard.onboard.rain_european_winter_storm import RAINEuropeanWinterStorm
 from hazard.models.water_temp import FutureStreamsSource, WaterTemperatureAboveIndicator
 from hazard.models.wet_bulb_globe_temp import WetBulbGlobeTemperatureAboveIndicator
@@ -321,13 +324,13 @@ def test_onboard_coastalflood_tudelft(test_output_dir):
 @pytest.mark.skip(reason="on-boarding script")
 def test_onboard_flopros(test_output_dir):
     source_path = os.path.join(test_output_dir, "flopros")
-    source = FLOPROSFloodStandardOfProtectionSource(source_path) 
+    source = FLOPROSFloodStandardOfProtectionSource(source_path)
     model = FLOPROSFloodStandardOfProtection()
     store = zarr.DirectoryStore(os.path.join(test_output_dir, "hazard", "hazard.zarr"))
     target = OscZarr(store=store)
     model.run_all(source, target)
     model.create_maps(target, target)
-    
+
 
 @pytest.mark.skip(reason="on-boarding script")
 def test_litpop(test_output_dir):
